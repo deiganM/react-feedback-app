e09e48835eee24b7012debc692a0763e5ceace0a
Deigan Millikin <deiganmillikin@gmail.com>
2023-06-16 14:16:49 -0600

    Added React router 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from './pages/AboutPage'
 
 function App() {
   // App level state
@@ -25,18 +27,28 @@ function App() {
   }
 
   return (
-    <>
-      {/* <Header text='Hello World' /> 
-        - <Header /> is using a default prop now
-      */}
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        {/* To add state to the 'FeedbackList' component, create a prop */}
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                {/* To add state to the 'FeedbackList' component, create a prop */}
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
